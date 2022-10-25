d5dfd97cf23a90334656ec8c4dfad43863915760
Clemens Schmid <clemens@nevrome.de>
2022-10-25 22:13:28 +0200

    good progress with the zoom functionality

--- elmsperiment/src/TeachingMaterial.elm
+++ elmsperiment/src/TeachingMaterial.elm
@@ -6,16 +6,19 @@ import MapOfComputionalArchaeology exposing (comparchmap)
 import Browser
 import Chart as C
 import Chart.Attributes as CA
+import Chart.Events as CE
+import Chart.Item as CI
 import Chart.Svg as CS
 import Svg as S
 import Svg.Attributes as SA
-import Html exposing (Html, div, h1, input, text, button, p)
-import Html.Attributes exposing (placeholder, style)
-import Html.Events exposing (onInput)
+import Html as H exposing (Html, div, h1, input, text, button, p)
+import Html.Attributes as HA exposing (placeholder, style)
+import Html.Events as HE exposing (onInput)
 import Table
 import Task
 import Csv.Decode as Decode exposing (Decoder)
 import List exposing (map)
+import VirtualDom exposing (attribute)
 
 main =
     Browser.element
@@ -25,18 +28,23 @@ main =
         , subscriptions = \_ -> Sub.none
         }
 
-
-
 -- MODEL
-
 
 type alias Model =
     { elements : List TeachingResource
     , testString : String
     , tableState : Table.State
     , query : String
+    -- mapZoom
+    , center : CS.Point
+    , dragging : Dragging
+    , percentage : Float
     }
 
+type Dragging
+  = CouldStillBeClick CS.Point
+  | ForSureDragging CS.Point
+  | None
 
 init : List TeachingResource -> ( Model, Cmd Msg )
 init elements =
@@ -46,29 +54,33 @@ init elements =
             , testString = ""
             , tableState = Table.initialSort "Year"
             , query = ""
+            -- mapZoom
+            , center = { x = 100, y = 50 }
+            , dragging = None
+            , percentage = 100
             }
     in
     ( model, Cmd.none )
 
-
-
 -- UPDATE
-
 
 type Msg
     = SetQuery String
     | SetTableState Table.State
+    -- mapZoom
+    | OnMouseDown CS.Point
+    | OnMouseMove CS.Point
+    | OnMouseUp CS.Point CS.Point
+    | OnMouseLeave
+    | OnZoomIn
+    | OnZoomOut
+    | OnZoomReset
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         
---        LoadCsvFile newList ->
---           ( { model | people = newList }
---           , Cmd.none 
---           )
-
         SetQuery newQuery ->
             ( { model | query = newQuery }
             , Cmd.none
@@ -79,13 +91,61 @@ update msg model =
             , Cmd.none
             )
 
+        -- mapZoom
+        OnMouseDown offset ->
+          ({ model | dragging = CouldStillBeClick offset }, Cmd.none)
 
+        OnMouseMove offset ->
+          case model.dragging of
+            CouldStillBeClick prevOffset ->
+              if prevOffset == offset then
+                (model, Cmd.none)
+              else
+                ( { model | center = updateCenter model.center prevOffset offset
+                , dragging = ForSureDragging offset
+                }, Cmd.none )
+            ForSureDragging prevOffset ->
+              ( { model | center = updateCenter model.center prevOffset offset
+              , dragging = ForSureDragging offset
+              }, Cmd.none )
+            None ->
+              (model, Cmd.none)
+
+        OnMouseUp offset coord ->
+          case model.dragging of
+            CouldStillBeClick prevOffset ->
+              ({ model | center = coord, dragging = None }, Cmd.none)
+            ForSureDragging prevOffset ->
+              ( { model | center = updateCenter model.center prevOffset offset
+              , dragging = None
+              }, Cmd.none )
+            None ->
+              (model, Cmd.none)
+
+        OnMouseLeave ->
+          ({ model | dragging = None }, Cmd.none)
+
+        OnZoomIn ->
+          ({ model | percentage = model.percentage + 20 }, Cmd.none)
+
+        OnZoomOut ->
+          ({ model | percentage = max 1 (model.percentage - 20) }, Cmd.none)
+
+        OnZoomReset ->
+          ({ model | percentage = 100, center = { x = 0, y = 0 } }, Cmd.none)
+
+
+updateCenter : CS.Point -> CS.Point -> CS.Point -> CS.Point
+updateCenter center prevOffset offset =
+  { x = center.x + (prevOffset.x - offset.x)
+  , y = center.y + (prevOffset.y - offset.y)
+  }
 
 -- VIEW
 
 
 view : Model -> Html Msg
-view { elements, testString, tableState, query } =
+view { elements, testString, tableState, query, center, dragging, percentage } =
     let
         lowerQuery =
             String.toLower query
@@ -95,17 +155,57 @@ view { elements, testString, tableState, query } =
 
         mapPlot = 
             C.chart
-                [ CA.height 300
-                , CA.width 600
+                [ CA.height 270
+                , CA.width 492
+                , CA.range [ CA.zoom percentage, CA.centerAt center.x ]
+                , CA.domain [ CA.zoom percentage, CA.centerAt center.y ]
+                , CE.onMouseDown OnMouseDown CE.getOffset
+                , CE.onMouseMove OnMouseMove CE.getOffset
+                , CE.on "mouseup" (CE.map2 OnMouseUp CE.getOffset CE.getCoords)
+                , CE.onMouseLeave OnMouseLeave
+                , CA.htmlAttrs
+                    [ HA.style "user-select" "none"
+                    , HA.style "cursor" <|
+                        case dragging of
+                          CouldStillBeClick _ -> "grabbing"
+                          ForSureDragging _ -> "grabbing"
+                          None -> "grab"
+                    ]
                 ]
-                [ C.xLabels [ CA.withGrid ]
-                , C.yLabels [ CA.withGrid ]
-                , C.svgAt .min .max 0 0 [ comparchmap [] ]
+                [ C.xLabels [ CA.withGrid, CA.amount 10, CA.ints, CA.fontSize 9 ]
+                , C.yLabels [ CA.withGrid, CA.amount 10, CA.ints, CA.fontSize 9 ]
+                , C.xTicks [ CA.amount 10, CA.ints ]
+                , C.yTicks [ CA.amount 10, CA.ints ]
+                , C.htmlAt .max .max 0 0
+                    [ HA.style "transform" "translateX(-100%)" ]
+                    [ H.span
+                        [ HA.style "margin-right" "5px" ]
+                        [ H.text (String.fromFloat percentage ++ "%") ]
+                    , H.button
+                        [ HE.onClick OnZoomIn
+                        , HA.style "margin-right" "5px"
+                        ]
+                        [ H.text "+" ]
+                    , H.button
+                        [ HE.onClick OnZoomOut
+                        , HA.style "margin-right" "5px"
+                        ]
+                        [ H.text "-" ]
+                    , H.button
+                        [ HE.onClick OnZoomReset ]
+                        [ H.text "⨯" ]
+                    ]
+
+                , C.svgAt (\_ -> 0) (\_ -> 100) 0 0 [ comparchmap [
+                        attribute "width" (String.fromFloat (600 * (percentage / 100)))
+                      , attribute "height" (String.fromFloat (300 * (percentage / 100)))
+                      , attribute "viewBox" ("0 0 2000 1000")
+                    ] ]
                 , C.series .x [ C.scatter .y [] ] elements
                 ]
     in
     div []
-        [ div [ style "margin" "auto", style "width" "50%", style "border" "3px solid green" ] [ mapPlot ]
+        [ div [ style "overflow" "hidden", style "margin" "auto", style "width" "40%", style "border" "3px solid green" ] [ mapPlot ]
         , h1 [] [ text "Teaching material list" ]
         , input [ placeholder "Search by Name", onInput SetQuery ] []
         , Table.view config tableState acceptablePeople
