7043e15c9d18ff246c1223030e6feda61c337e26
Clemens Schmid <clemens@nevrome.de>
2022-12-09 12:16:33 +0100

    added teaching material edge list to elm code

--- script_teachingmaterial_tsv2elm.sh
+++ script_teachingmaterial_tsv2elm.sh
@@ -2,9 +2,18 @@
 
 # teaching material tsv to elm code
 cp data/teachingmaterial.tsv src/TeachingMaterialData.elm
-prepent='module TeachingMaterialData exposing (teachingMaterialString)
+prepent1='module TeachingMaterialData exposing (teachingMaterialString)
 \n 
 \nteachingMaterialString : String
 \nteachingMaterialString = """'
-echo -en $prepent | cat - src/TeachingMaterialData.elm > temp && mv temp src/TeachingMaterialData.elm
+echo -en $prepent1 | cat - src/TeachingMaterialData.elm > temp && mv temp src/TeachingMaterialData.elm
 echo '"""' >> src/TeachingMaterialData.elm
+
+# teaching material edgelist tsv to elm code
+cp data/edgelist_teachingmaterial.tsv src/TeachingMaterialEdges.elm
+prepent2='module TeachingMaterialData exposing (teachingMaterialEdgesString)
+\n 
+\nteachingMaterialEdgesString : String
+\nteachingMaterialEdgesString = """'
+echo -en $prepent2 | cat - src/TeachingMaterialEdges.elm > temp && mv temp src/TeachingMaterialEdges.elm
+echo '"""' >> src/TeachingMaterialEdges.elm
